79cf55ff7783c9c1123c9a93ab805c682524b50e
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2018-02-07 14:17:19 +0700

    Demo thư viện React: Material UI, tạo component MaterialDatePicker, import css cho component.

---
+++ VIVUTravelAgency/ClientApp/components/Demo/MaterialDatePicker.tsx
@@ -0,0 +1,26 @@
+﻿//ReSharper disable All
+import * as React from 'react';
+import DatePicker from 'material-ui/DatePicker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import "../../css/demo.css"
+
+export class MaterialDatePicker extends React.Component<{ message: string, onDateChanged:any}, {}> {
+    public constructor(prop: any) {
+        super(prop);
+    }
+
+    public render() {
+        return <div>
+            <span>
+                <label>{this.props.message}</label>
+            </span>
+            <MuiThemeProvider>
+                <DatePicker hintText="Xin hãy nhấn vào đây" mode="landscape" className="demoDatePicker" onChange={this.DateChanged.bind(this)} />
+            </MuiThemeProvider>
+        </div>;
+    }
+
+    public DateChanged(o: undefined, date: object) {
+        this.props.onDateChanged(date);
+    }
+}
--- VIVUTravelAgency/ClientApp/components/Home.tsx
+++ VIVUTravelAgency/ClientApp/components/Home.tsx
@@ -1,11 +1,16 @@
-import * as React from 'react';
+﻿import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import {MaterialDatePicker} from "./Demo/MaterialDatePicker";
 
 declare global {
     interface Window { lstMem: any }
 }
 
 export class Home extends React.Component<RouteComponentProps<{}>, {}> {
+    public constructor() {
+        super();
+        this.selectedAlert.bind(this);
+    }
     public render() {
         return <div>
                    <div>
@@ -15,6 +20,13 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                        <label>List of members: </label>
                    </div>
                    {window.lstMem.map(ele => <label>{ele}</label>)}
+                   <div>
+                <MaterialDatePicker message="Xin hãy nhập ngày tháng năm sinh: " onDateChanged={this.selectedAlert}/>
+                </div>
                </div>;
+    }
+
+    selectedAlert(selectedDate : string) {
+        alert(selectedDate);
     }
 }
