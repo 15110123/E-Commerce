b39ee40b320e0f3e89b0309f07053095ff3a5753
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2018-03-15 23:01:02 +0700

    [HTML]: Khởi tạo component PopularPlace (trang chủ)

---
+++ StaticWebTest/Components/PopularPlace/popularplace.js
@@ -0,0 +1,69 @@
+// var init = () => {
+//     $(".star1").on('mouseover', (e) => {
+//         console.log(e.target.parentNode.parentNode.parentNode);
+//         for (var j = 1; j <= 1; j++) {
+//             let preStarClassName = '.star' + j;
+//             $(preStarClassName).foreach((o, i, a) => {
+//                 $(preStarClassName)[0].src = "/img/svg/yellowStarIcon.svg";
+//             });
+//         }
+//     });
+//     $(".star1").on('mouseleave', () => {
+//         for (var j = 1; j <= 1; j++) {
+//             let preStarClassName = '.star' + j;
+//             $(preStarClassName)[0].src = "/img/svg/grayStarIcon.svg";
+//         }
+//     });
+
+//     $(".star2").on('mouseover', () => {
+//         for (var j = 1; j <= 2; j++) {
+//             let preStarClassName = '.star' + j;
+//             $(preStarClassName)[0].src = "/img/svg/yellowStarIcon.svg";
+//         }
+//     });
+//     $(".star2").on('mouseleave', () => {
+//         for (var j = 1; j <= 2; j++) {
+//             let preStarClassName = '.star' + j;
+//             $(preStarClassName)[0].src = "/img/svg/grayStarIcon.svg";
+//         }
+//     });
+
+//     $(".star3").on('mouseover', () => {
+//         for (var j = 1; j <= 3; j++) {
+//             let preStarClassName = '.star' + j;
+//             $(preStarClassName)[0].src = "/img/svg/yellowStarIcon.svg";
+//         }
+//     });
+//     $(".star3").on('mouseleave', () => {
+//         for (var j = 1; j <= 3; j++) {
+//             let preStarClassName = '.star' + j;
+//             $(preStarClassName)[0].src = "/img/svg/grayStarIcon.svg";
+//         }
+//     });
+
+//     $(".star4").on('mouseover', () => {
+//         for (var j = 1; j <= 4; j++) {
+//             let preStarClassName = '.star' + j;
+//             $(preStarClassName)[0].src = "/img/svg/yellowStarIcon.svg";
+//         }
+//     });
+//     $(".star4").on('mouseleave', () => {
+//         for (var j = 1; j <= 4; j++) {
+//             let preStarClassName = '.star' + j;
+//             $(preStarClassName)[0].src = "/img/svg/grayStarIcon.svg";
+//         }
+//     });
+
+//     $(".star5").on('mouseover', () => {
+//         for (var j = 1; j <= 5; j++) {
+//             let preStarClassName = '.star' + j;
+//             $(preStarClassName)[0].src = "/img/svg/yellowStarIcon.svg";
+//         }
+//     });
+//     $(".star5").on('mouseleave', () => {
+//         for (var j = 1; j <= 5; j++) {
+//             let preStarClassName = '.star' + j;
+//             $(preStarClassName)[0].src = "/img/svg/grayStarIcon.svg";
+//         }
+//     });
+// }
